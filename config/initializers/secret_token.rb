cf563da3152179d9999317398a8addd2737b089e
zorch <zorch.mcclaud.da@gmail.com>
2013-04-11 09:26:06 -0500

    Subiendo por primera vez el proyecto con carpetas inicializadas

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Adan::Application.config.secret_token = 'd93b23ea1d884250be7dad3e9d1f8c844d08424a31317e5faa479e593ffececc7658c69e79ff0b5cfe1da0e78de18ba39f1792388c494ea92d43211e24be8162'
